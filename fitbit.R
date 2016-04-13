95e0360d3e7e86cd26edc05d587566cabc8ccfb9
Zach <zach@wayfaring.io>
2016-04-12 17:58:52 -0700

    Updated authorization URL

--- fitbit.R
+++ fitbit.R
@@ -1,10 +1,12 @@
 library(httr)
 library(xml2)
+library(jsonlite)
 
 fitbit_endpoint <- oauth_endpoint(
   request = "https://api.fitbit.com/oauth2/token",
-  authorize = "http://localhost:1410/", #loopback
+  authorize = "https://www.fitbit.com/oauth2/authorize",
   access = "https://api.fitbit.com/oauth2/token")
+  
 myapp <- oauth_app(
   appname = "data_access",
   key = key, #my consumer key
@@ -13,10 +15,8 @@ myapp <- oauth_app(
 # 2. Get OAuth token
 scope <- c("activity")  # See dev.fitbit.com/docs/oauth2/#scope
 fitbit_token <- oauth2.0_token(fitbit_endpoint, myapp,
-                               scope = scope, use_basic_auth = TRUE,
-                               user_params = fitbit_endpoint)
-
+                               scope = scope, use_basic_auth = TRUE)
 # 3. Make API requests
-resp <- GET(url = "https://api.fitbit.com/1/user/-/activities/activity/date/2016-01-01/2016-04-09.json", 
+resp <- GET(url = "https://api.fitbit.com/1/user/-/activities/steps/date/today/1m.json", 
             config(token = fitbit_token))
 content(resp)
